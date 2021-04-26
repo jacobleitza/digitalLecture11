255aa46eb074bd645f719bcea106de1e10683807
jacobleitza <jacobleitza@gmail.com>
2021-04-26 13:19:27 -0400

    added another led

--- digitalLecture11.ino
+++ digitalLecture11.ino
@@ -1,12 +1,16 @@
 int ledPin = 24;
+int ledPin2 = 25;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
   delay(500);
   digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin2, LOW);
   delay(500);  
 }
